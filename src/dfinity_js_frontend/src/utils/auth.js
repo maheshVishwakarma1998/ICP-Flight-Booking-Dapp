e473bcd10a126e11c1cb916eb4e966cd01b350ce
mahesh <ishwakam@gmail.com>
2024-03-18 22:24:14 -0700

    wip

--- src/dfinity_js_frontend/src/utils/auth.js
+++ src/dfinity_js_frontend/src/utils/auth.js
@@ -1,8 +1,9 @@
 import { AuthClient } from "@dfinity/auth-client";
 
 // that is the url of the webapp for the internet identity. 
-const IDENTITY_PROVIDER = `http://localhost:4943/?canisterId=bd3sg-teaaa-aaaaa-qaaba-cai#authorize`;
+const IDENTITY_PROVIDER = `http://localhost:4943/?canisterId=rdmx6-jaaaa-aaaaa-aaadq-cai#authorize`;
 const MAX_TTL = 7 * 24 * 60 * 60 * 1000 * 1000 * 1000;
+// const IDENTITY_PROVIDER = `http://rdmx6-jaaaa-aaaaa-aaadq-cai.localhost:8080/`;
 
 export async function getAuthClient() {
     return await AuthClient.create();
